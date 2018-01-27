b627d9dd24d210563b95d72adf2ea95206e061e9
weizhengda <2625785357@qq.com>
2018-01-27 22:56:54 +0800

    1.27

---
+++ code/12.第十二章/1.lambda表达式/code.py
@@ -0,0 +1,12 @@
+# 匿名函数
+
+def add(x,y):
+    return x + y
+
+f = lambda x,y:x+y
+print(f(1,2))
+
+# lambada 表达式
+lambda parameter_list:expression
+
+
---
+++ code/12.第十二章/2.三元表达式/code.py
@@ -0,0 +1,6 @@
+
+# 条件为真时返回的结果 if 条件判断 else 条件为假时的返回结果
+x = 2
+y = 1
+r = x if x > y else y
+print(r) # 2
